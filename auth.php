64392f64d236d4c717ce6b94a33d0b7dc199f7b0
nlaber <36971532+nlaber@users.noreply.github.com>
2018-03-20 19:49:55 +0300

    123

---
+++ auth.php
@@ -0,0 +1,20 @@
+﻿
+<form action=index.html method=POST>
+<input type=text name=pas>
+<input type=submit>
+</form>
+
+<?PHP 
+if (isset($_POST['pas']))
+{
+ if ($_POST['pas'] == '123')
+   {
+     include("vk.com");
+   }
+   else
+   {
+     echo "access denied";
+   } 
+}
+?>
+
