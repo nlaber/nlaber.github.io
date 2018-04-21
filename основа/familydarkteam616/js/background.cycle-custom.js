0741f60b5e5c8a116cde510314b0fecc96d47fe3
nlaber <36971532+nlaber@users.noreply.github.com>
2018-04-22 00:02:57 +0300

    global update

---
+++ основа/familydarkteam616/js/background.cycle-custom.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/slider-image-1.jpg',
+
+'images/slider-image-3.jpg',
+
+
+'images/slider-image-5.jpg',
+'images/slider-image-4.jpg',
+			 'images/slider-image-2.jpg']
+
+
+
+
+
+
+
+});
